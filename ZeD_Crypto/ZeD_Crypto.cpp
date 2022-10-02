4ee60f82a1a2da53b449bfabf585fdc36a6c3675
ArtZetD <56314346+ArtZetD@users.noreply.github.com>
2022-10-02 15:37:59 +0300

    find removable drive name

--- ZeD_Crypto/ZeD_Crypto.cpp
+++ ZeD_Crypto/ZeD_Crypto.cpp
@@ -6,6 +6,26 @@
 
 int main()
 {
+	TCHAR szBuffer[256];
+	LPCTSTR bufferPtr = szBuffer;
+	DWORD d_logical_drive_string;
+	GetLogicalDriveStrings(255, szBuffer);
 
+	int counter = 0;
+
+
+	for (bufferPtr; *bufferPtr != '\0'; bufferPtr += _tcsclen(szBuffer) + 1)
+	{
+
+		if (GetDriveType(bufferPtr) == DRIVE_REMOVABLE)
+		{
+			std::cout << "the removable drive has root name - ";
+			std::wcout << bufferPtr << std::endl;
+			static_cast<char>(*bufferPtr);
+		}
+
+
+	}
+	std::cout << counter;
 }
 
